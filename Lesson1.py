67cf34784bcc389f4be79feb97336a3a94a4e569
srcanyildiz <srcanyildiz@gmail.com>
2019-09-29 17:22:24 +0300

    Last changed

--- Lesson1.py
+++ Lesson1.py
@@ -8,5 +8,20 @@ print("islem sonucu:",sayi)
 print(benimAdim + " " + benimSoyadim)
 
 full = benimAdim + " " + benimSoyadim
-print("Kullanicinin adi soyadi:",fullname)
+print("Kullanicinin adi soyadi:",full)
 
+print("bige\nadam")
+print ("""
+bilge
+adam
+merhaba
+sercan""")
+
+metin = "\"Bilge Adam\" besiktas subesi \"Python\" dersleri\n"
+print(metin * 2)
+
+sayi1 = 50
+sayi2 = 120
+toplam = sayi1 + sayi2
+print("Toplama işleminin sonucu: ",toplam)
+print("Toplama işleminin sonucu: " + str(toplam))
