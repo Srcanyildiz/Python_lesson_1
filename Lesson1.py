3ab5451c9b3f6b5e9b9658776dcd38a4713cf90f
srcanyildiz <srcanyildiz@gmail.com>
2019-09-29 16:58:00 +0300

    Last changed

---
+++ Lesson1.py
@@ -0,0 +1,12 @@
+benimAdim ="Sercan"
+benimSoyadim = "Yıldız"
+
+print(benimAdim)
+
+sayi = 5
+print("islem sonucu:",sayi)
+print(benimAdim + " " + benimSoyadim)
+
+full = benimAdim + " " + benimSoyadim
+print("Kullanicinin adi soyadi:",fullname)
+
