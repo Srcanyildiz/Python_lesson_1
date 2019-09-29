096583b5cf5d0c08f762f377b7b428a439776c1b
srcanyildiz <srcanyildiz@gmail.com>
2019-09-29 16:17:56 +0300

    Değiskenler Örneği

---
+++ Degiskenler.py
@@ -0,0 +1,11 @@
+benimAdim ="Sercan"
+benimSoyadim = "Yıldız"
+
+print(benimAdim)
+
+sayi = 5
+print("islem sonucu:",sayi)
+print(benimAdim + " " + benimSoyadim)
+
+fullname = benimAdim + " " + benimSoyadim
+print("Kullanicinin adi soyadi:",fullname)
